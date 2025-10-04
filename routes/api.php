75e579ca1c450a14f3d3db2865df46b09e4c756f
birsenaydin <bsnaydin@gmail.com>
2025-10-04 14:14:11 +0300

    chore(bootstrap): laravel + sanctum + permission + horizon; routes split

---
+++ routes/api.php
@@ -0,0 +1,3 @@
+<?php
+use Illuminate\Support\Facades\Route;
+Route::prefix('api/v1')->group(function(){});
---
+++ routes/author.php
@@ -0,0 +1,3 @@
+<?php
+use Illuminate\Support\Facades\Route;
+Route::middleware(['web'])->prefix('author')->group(function(){});
